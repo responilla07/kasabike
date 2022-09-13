4b9b498d99edd36b4e19bda99fe3f4228447ccda
MaureenDadap <maureendadap@gmail.com>
2022-09-13 14:24:10 +0800

    fixed pubspec

---
+++ lib/pages/login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  // const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            child: Row(
+              children: [],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
