4ace89d0a97ee509f2aad179c4dc4e3900ac98c8
MaureenDadap <maureendadap@gmail.com>
2022-09-13 16:45:57 +0800

    fixed simple logo

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kasabike/pages/login.dart';
 
 class IndexPage extends StatefulWidget {
   @override
@@ -10,7 +11,13 @@ class IndexPageState extends State<IndexPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Text('Hello index'),
+        child: TextButton(
+          child: Text('Hello index'),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Login()));
+          },
+        ),
       ),
     );
   }
