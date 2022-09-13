da92e4bb33c47fe33d40ddd7494d7d4f8e8a6ae8
xiwbo <bowwikatigbak16@gmail.com>
2022-09-13 19:41:59 +0800

    added google auth

---
+++ lib/pages/chat.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Hello chat'),
+      ),
+    );
+  }
+}
+
